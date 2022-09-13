73a46bf8224342627f8227749eb5d5847174b05d
Sanjeev Kumar <kumar.san96@gmail.com>
2022-09-13 16:25:41 +0200

    added widgets to select csv

--- src/napari_features_selection/_widget.py
+++ src/napari_features_selection/_widget.py
@@ -6,41 +6,48 @@ see: https://napari.org/stable/plugins/guides.html?#widgets
 
 Replace code below according to your needs.
 """
+import numpy as np
+import pandas as pd
+# from magicgui.widgets import create_widget
 from typing import TYPE_CHECKING
-
 from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget, QLabel, QVBoxLayout
 
 if TYPE_CHECKING:
     import napari
 
-
-class ExampleQWidget(QWidget):
+class FeaturesSelection(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
     # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
     def __init__(self, napari_viewer):
         super().__init__()
+
         self.viewer = napari_viewer
+        self.create_layout()
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+    
+    def create_layout(self):
+        self.worker = None
+        self.setLayout(QVBoxLayout())
+        #for title 
+        title_container = QWidget()
+        title_container.setLayout(QVBoxLayout())
+        title_container.layout().addWidget(QLabel("<b>Input Features CSV File</b>"))
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+        #for path selection widget and buttons
+        path_selection_container = QWidget()
+        path_selection_container.setLayout(QHBoxLayout())
+        btn = QPushButton("Select")
+        path_selection_container.layout().addWidget(QLabel("File Path"))
+        path_selection_container.layout().addWidget(btn)
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+        self.layout().addWidget(title_container)
+        self.layout().addWidget(path_selection_container)
 
 
+#need to find out its utility
 @magic_factory
 def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
-
-
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+    pass
